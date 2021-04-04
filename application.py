42275295706c62ca6ff168c140be3e94cdd6df7e
angeuwase <angeuwasedev@gmail.com>
2021-04-04 05:04:58 +0100

    changed flask instance name to application and module to application.py

--- application.py
+++ application.py
@@ -2,11 +2,11 @@ from project import create_app, db
 #from project import db
 #from project.models import User, Task
 
-app = create_app()
+application = create_app()
 
 #@app.shell_context_processor
 #def make_shell_context():
 #    return dict(db=db, User=User, Task=Task)
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
